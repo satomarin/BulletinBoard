f79c4fa3f06ae5a4c6d1a971225ff10dd54e0dce
sato marin <uwwork15@gmail.com>
2016-11-09 18:28:25 +0900

    three

--- src/bulletinboard/dao/UserDao.java
+++ src/bulletinboard/dao/UserDao.java
@@ -15,7 +15,7 @@ import bulletinboard.exception.SQLRuntimeException;
 
 public class UserDao {
 
-
+	//ログイン用
 	public User getUser(Connection connection, String account, String password) {
 
 		PreparedStatement ps = null;
@@ -43,6 +43,7 @@ public class UserDao {
 		}
 	}
 
+	//新規登録用
 	private List<User> toUserList(ResultSet rs) throws SQLException {
 
 		List<User> ret = new ArrayList<User>();
@@ -75,7 +76,7 @@ public class UserDao {
 		}
 	}
 
-
+	//新規登録で打ち込んだデータをdbへ
 	public void insert (Connection connection, User user) {
 
 		PreparedStatement ps = null;
@@ -107,7 +108,102 @@ public class UserDao {
 			ps.setString(4, user.getBranchID());
 			ps.setString(5, user.getDepartmentID());
 
-			System.out.println(ps.toString());
+			ps.executeUpdate();
+
+
+		} catch (SQLException e) {
+			throw new SQLRuntimeException(e);
+		} finally {
+			close(ps);
+		}
+	}
+
+	//全てのユーザーテーブルのデータをもらう
+	public List<User> getUser(Connection connection) {
+
+		PreparedStatement ps = null;
+		try {
+			StringBuilder sql = new StringBuilder();
+			//SQL文を用意
+			sql.append("SELECT * FROM users ");
+			//生成
+			ps = connection.prepareStatement(sql.toString());
+
+			//実行
+			ResultSet rs = ps.executeQuery();
+			//listの対象の型に落としこみ
+			List<User> ret = toUserList(rs);
+
+			return ret;
+
+		} catch (SQLException e) {
+			throw new SQLRuntimeException(e);
+
+		} finally {
+			close(ps);
+		}
+	}
+
+	//編集時に選択されたユーザーIDのデータをもらう
+	public User getUser(Connection connection, String editId) {
+
+		PreparedStatement ps = null;
+		try {
+			String sql = "SELECT * FROM users WHERE id = ?";
+
+			ps = connection.prepareStatement(sql);
+			ps.setString(1, editId);
+
+
+			ResultSet rs = ps.executeQuery();
+			List<User> userList = toUserList(rs);
+
+			if (userList.isEmpty() == true) {
+				return null;
+			}else {
+				return userList.get(0);
+			}
+
+		} catch (SQLException e) {
+			throw new SQLRuntimeException(e);
+		} finally {
+			close(ps);
+		}
+
+	}
+
+	//ユーザー情報更新
+	public void update (Connection connection, User user) {
+
+		PreparedStatement ps = null;
+		try{
+			StringBuilder sql = new StringBuilder();
+			sql.append("UPDATE users SET ");
+			sql.append(" account");
+			sql.append(" = ? "); // account
+			sql.append(", name");
+			sql.append(" = ? "); // name
+			sql.append(", password");
+			sql.append(" = ? "); // password
+			sql.append(", branch_id");
+			sql.append(" = ? "); // branch_id
+			sql.append(", department_id");
+			sql.append(" = ? "); // department_id
+			sql.append(", update_date");
+			sql.append(" = CURRENT_TIMESTAMP "); // update_date
+
+			sql.append("WHERE id = ? ");
+
+			ps = connection.prepareStatement(sql.toString());
+
+			System.out.println(ps);
+
+			ps.setString(1, user.getAccount());
+			ps.setString(2, user.getPassword());
+			ps.setString(3, user.getName());
+			ps.setString(4, user.getBranchID());
+			ps.setString(5, user.getDepartmentID());
+			ps.setInt(6, user.getId());
 
 			ps.executeUpdate();
 
--- src/bulletinboard/service/UserService.java
+++ src/bulletinboard/service/UserService.java
@@ -4,6 +4,7 @@ import static bulletinboard.utils.CloseableUtil.*;
 import static bulletinboard.utils.DBUtils.*;
 
 import java.sql.Connection;
+import java.util.List;
 
 import bulletinboard.beans.User;
 import bulletinboard.dao.UserDao;
@@ -11,12 +12,12 @@ import bulletinboard.utils.CipherUtil;
 
 public class UserService {
 
+	//ユーザー新規登録
 	public void register(User user){
 
 		Connection connection = null;
 		try {
 			connection = getConnection();
-			System.out.println(user.getPassword());
 
 			String encPassword = CipherUtil.encrypt(user.getPassword());
 			user.setPassword(encPassword);
@@ -35,4 +36,79 @@ public class UserService {
 			close(connection);
 		}
 	}
+
+	//ユーザー全体のデータを扱う
+	public List<User> getUser() {
+
+		Connection connection = null;
+		try {
+			connection = getConnection();
+
+			UserDao userDao = new UserDao();
+			List<User> ret = userDao.getUser(connection);
+
+			commit(connection);
+
+			return ret;
+		} catch (RuntimeException e) {
+			rollback(connection);
+			throw e;
+		} catch (Error e) {
+			rollback(connection);
+			throw e;
+		} finally {
+			close(connection);
+		}
+	}
+
+	//編集画面で選択されたユーザーidのデータを扱う
+	public User editing(String editId) {
+
+		Connection connection = null;
+		try {
+			connection = getConnection();
+
+			UserDao userDao = new UserDao();
+			User user = userDao.getUser(connection, editId);
+
+			commit(connection);
+
+			return user;
+		} catch (RuntimeException e) {
+			rollback(connection);
+			throw e;
+		} catch (Error e) {
+			rollback(connection);
+			throw e;
+		} finally {
+			close(connection);
+		}
+	}
+
+	//編集画面でユーザーデータを更新する用
+	public void update(User user) {
+
+		Connection connection = null;
+		try {
+			connection = getConnection();
+
+			String encPassword = CipherUtil.encrypt(user.getPassword());
+			user.setPassword(encPassword);
+
+			UserDao userDao = new UserDao();
+			userDao.update(connection, user);
+
+			commit(connection);
+		} catch (RuntimeException e) {
+			rollback(connection);
+			throw e;
+		} catch (Error e) {
+			rollback(connection);
+			throw e;
+		} finally {
+			close(connection);
+		}
+
+	}
+
 }
